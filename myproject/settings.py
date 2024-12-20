bdfe15e969f4e75f51c32cbdac504d6ac57b509f
karthick-g07 <karthickfrank007@gmail.com>
2024-12-20 15:03:17 +0530

    last modified

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-9*twy@9kb!k4n1o=9*fyh@xaa-j$ori#n0fp7t$c#$i=gjjwvu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 '''STATIC_URL = '/static/'
@@ -81,8 +82,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -117,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL='static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- useractivities/dboperations.py
+++ useractivities/dboperations.py
@@ -5,11 +5,9 @@ client = MongoClient('mongodb+srv://karthick:123@cluster0.fzmnu.mongodb.net/?aut
 db = client['test']  # Replace with your database name
 collection = db['users']  # Replace with your collection name
 
-# Check if the connection is established
 try:
-    # Attempt to send a ping to the MongoDB server
     client.admin.command('ping')
-    print("MongoDB connection established successfully!")
+   
 except Exception as e:
     print(f"Error connecting to MongoDB: {e}")
 
@@ -18,7 +16,6 @@ def insertdata(user_data):
     result = collection.insert_one(user_data)
     
 
-# Retrieve all users to check if the email exists
 def get_all_users():
     users = collection.find()  # Fetch all users from the database
     return list(users)  # Return a list of users
@@ -36,4 +33,4 @@ def get_user_by_email(email):
 def update_user_password(email, new_password):
     """Update the user's password in MongoDB."""
     result = collection.update_one({'email': email}, {'$set': {'password': new_password}})
-    return result.modified_count > 0  # Returns True if a document was updated
+    return result.modified_count > 0  
